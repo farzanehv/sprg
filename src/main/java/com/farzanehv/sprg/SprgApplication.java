dc6489268fb78fa9bfee07954a08a4ae89ef2fb3
Farzaneh Varaminian <Farzaneh@Farzanehs-MacBook-Pro.local>
2020-03-13 19:35:21 +0330

    empty spring boot project

---
+++ src/main/java/com/farzanehv/sprg/SprgApplication.java
@@ -0,0 +1,13 @@
+package com.farzanehv.sprg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farzanehv/sprg/SprgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farzanehv.sprg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
